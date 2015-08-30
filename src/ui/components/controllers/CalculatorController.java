e2701ccc564a40e7048dcb00e76f78c1f2772b48
Shane Richards <shane.richards121@gmail.com>
2015-08-30 18:28:00 -0500

    calculator has basic functionality

--- src/ui/components/controllers/CalculatorController.java
+++ src/ui/components/controllers/CalculatorController.java
@@ -1,11 +1,14 @@
 package ui.components.controllers;
 
+import function.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 /**
@@ -15,13 +18,47 @@ public class CalculatorController implements Initializable {
 
     @FXML private TextField displayField;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    private String lastInput;
+    private static boolean errorStatus;
+
+    @Override public void initialize(URL location, ResourceBundle resources) {
         // TODO
     }
 
-    @FXML public void handleZeroAction(ActionEvent event){
-        System.out.println("0 pressed");
+
+    @FXML public void handleOperandAction(ActionEvent actionEvent) {
+        String digit = ((Button) actionEvent.getSource()).getText();
+        String equation = displayField.getText();
+
+        equation += digit;
+
+        displayField.setText(equation);
     }
 
+    @FXML public void handleOperatorAction(ActionEvent actionEvent) {
+        String operator = ((Button) actionEvent.getSource()).getText();
+        String equation = displayField.getText();
+
+        equation += " " + operator + " ";
+
+        displayField.setText(equation);
+    }
+
+    @FXML public void handleEqualAction(ActionEvent actionEvent) {
+        String expression = displayField.getText();
+
+        String[] tokens = FunctionParser.tokenize(expression);
+
+        System.out.println(Arrays.toString(tokens));
+
+        Tree tree = FunctionAdapter.newInstance(tokens).buildTree();
+
+        if (tree instanceof NullTree) {
+            System.out.println("Error");
+            displayField.setText("ERROR");
+        } else {
+            ((FunctionTree) tree).postOrder();
+            displayField.setText(((FunctionTree) tree).execute());
+        }
+    }
 }
